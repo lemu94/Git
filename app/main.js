e6242289960a1916b515f17079d301ebeeedaca5
YOKOI KPAMISSA LEMUEL-ANGE <lyokoi@reference.ca>
2025-05-18 13:34:38 -0400

    fix: clean up output by trimming trailing whitespace in 'cat-file' command

--- app/main.js
+++ app/main.js
@@ -35,7 +35,7 @@ console.error("Logs from your program will appear here!");
 
               // Récupère uniquement le contenu après le \x00
               const contenu = result.slice(idx + 1).toString('utf8');
-              const contenuNettoye = contenu.replace(/[\r\n]+$/, '');
+              const contenuNettoye = contenu.trimEnd();
               console.log(contenuNettoye);
               });
           });
